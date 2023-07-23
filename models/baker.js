d34b17d9d1b84bc18143be6e42663be9e39a1ed1
aminatol <aminatoluwo@gmail.com>
2023-07-23 13:28:45 -0400

    added baker first delete pre hook

--- models/baker.js
+++ models/baker.js
@@ -1,31 +1,35 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const Bread = require("./breads.js");
 
-const bakerSchema = new Schema({
-    name: { 
-        type: String,
-        required: true,
-        enum: [
-            "Monica", 
-            "Rachel", 
-            "Ross", 
-            "Joey", 
-            "Phoebe", 
-            "Chandler"
-        ]
+const bakerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      enum: ["Monica", "Rachel", "Ross", "Joey", "Phoebe", "Chandler"],
     },
-    startDate: {type: Date, required: true},
-    bio: {type: String}
-}, { toJSON: { virtuals: true } })
+    startDate: { type: Date, required: true },
+    bio: { type: String },
+  },
+  { toJSON: { virtuals: true } }
+);
 
 // Virtuals:
-bakerSchema.virtual('breads', {
-    ref: 'Bread',
-    localField: '_id',
-    foreignField: 'baker'
-})
+//equal to saying Bread.find({baker: this ParticularBaker.id})
+bakerSchema.virtual("breads", {
+  ref: "Bread", // look to bread model in Mongodb
+  localField: "_id", // look to id (in Mongodb)
+  foreignField: "baker", // link baker object id to bread id
+});
 
+//HOOKS
+bakerSchema.pre("findOneAndDelete", function () {
+  Bread.deleteMany({ id: this._conditions._id }).then((deletedBreads) => {
+    console.log(deletedBreads);
+  });
+});
 
-const Baker = mongoose.model('Baker', bakerSchema)
+const Baker = mongoose.model("Baker", bakerSchema);
 
-module.exports = Baker
+module.exports = Baker;
